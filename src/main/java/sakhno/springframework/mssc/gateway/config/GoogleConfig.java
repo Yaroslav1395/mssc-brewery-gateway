5738fc2f21268d2d213252e7519d8e15f835408a
Ярослав <sakhno.yaroslav.13.95@gmail.com>
2025-03-19 20:06:17 +0600

    коментарий

--- src/main/java/sakhno/springframework/mssc/gateway/config/GoogleConfig.java
+++ src/main/java/sakhno/springframework/mssc/gateway/config/GoogleConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GoogleConfig {
     /**
-     *
+     * Настройка маршрутизации
      * @param builder
      * @return
      */
