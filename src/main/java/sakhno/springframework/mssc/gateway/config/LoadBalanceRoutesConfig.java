59eb517135870e029f48b1e996dd570737a42f08
Yarnercom <a.doroev@mail.ru>
2025-03-21 18:33:36 +0600

    интреграция с эврика

--- src/main/java/sakhno/springframework/mssc/gateway/config/LoadBalanceRoutesConfig.java
+++ src/main/java/sakhno/springframework/mssc/gateway/config/LoadBalanceRoutesConfig.java
@@ -5,21 +5,20 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-public class LocalHostRouteConfig {
-
+@Configuration
+public class LoadBalanceRoutesConfig {
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalanceRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("beer-service", r -> r
                         .path("/api/v1/beer/**")
-                        .uri("http://localhost:8080"))
+                        .uri("lb://MSSC-BEER-SERVICE"))
                 .route("beer-order-service", r -> r
                         .path("/api/v1/customer/**")
-                        .uri("http://localhost:8081"))
+                        .uri("lb://MSSC-BEER-ORDER-SERVICE"))
                 .route("beer-inventory-service", r -> r
                         .path("/api/v1/inventory/beer/**")
-                        .uri("http://localhost:8082"))
+                        .uri("lb://MSSC-BEER-INVENTORY-SERVICE"))
                 .build();
     }
 }
